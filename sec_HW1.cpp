7c64776c81e71ed095053a3d87580452625a0d03
poiuy0308 <63567777+poiuy0308@users.noreply.github.com>
2020-04-27 21:58:45 +0900

    Add files via upload

---
+++ sec_HW1.cpp
@@ -0,0 +1,27 @@
+﻿// sec_HW1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    ifstream fin;
+    fin.open("Text.txt");
+
+    ofstream fout;
+    fout.open("copy.txt");
+
+    char ch;
+
+    while (!fin.eof())
+    {
+        fin.read(&ch, sizeof(ch));
+        fout.write(&ch, sizeof(ch));
+    }
+
+    fin.close();
+    fout.close();
+    return 0;
+}
---
+++ sec_HW2.cpp
@@ -0,0 +1,30 @@
+﻿// sec_HW2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <fstream>
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+	ifstream ifs;
+
+	ifs.open("Text.txt");
+
+	int sum[4] = { 0 };
+	int num[8] = { 0 };
+	int j = 0;
+
+	for (int i = 0; i < 8; i++) {
+		ifs >> num[i];
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		sum[i] = num[i * 2] + num[i * 2 + 1];
+		cout << sum[i] << endl;
+		//cout << i << " " << sum[i] << endl;
+	}
+	return 0;
+
+}
